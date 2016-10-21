4dcbf246c291beaa53524bf70c453e52622cd659
Joel Bout <joel@taotesting.com>
2016-10-21 17:44:16 +0200

    Bump to 0.6.0

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return array(
     'label' => 'Workspace',
     'description' => 'Supports workspaces for items',
     'license' => 'GPL-2.0',
-    'version' => '0.5.0',
+    'version' => '0.6.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'generis' => '>=3.1.0',
