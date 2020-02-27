09f54ca44b9dc97557ff692e7e2f152152abd9e9
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-02-27 11:13:21 +0300

    Fix usage of updated taoRevision ext

--- scripts/install/SetupWrapper.php
+++ scripts/install/SetupWrapper.php
@@ -20,13 +20,18 @@
  */
 namespace oat\taoWorkspace\scripts\install;
 
+use common_Exception;
 use common_persistence_Manager;
-use core_kernel_persistence_smoothsql_SmoothModel;
+use core_kernel_persistence_smoothsql_SmoothModel as SmoothModel;
+use oat\generis\model\data\Model;
 use oat\generis\model\data\ModelManager;
 use oat\generis\model\kernel\persistence\smoothsql\search\ComplexSearchService;
+use oat\generis\persistence\PersistenceManager;
 use oat\oatbox\extension\InstallAction;
+use oat\oatbox\service\exception\InvalidServiceManagerException;
 use oat\tao\model\lock\LockManager;
 use oat\taoRevision\model\Repository;
+use oat\taoRevision\model\RepositoryInterface;
 use oat\taoRevision\model\RepositoryService;
 use oat\taoWorkspace\model\generis\WrapperModel;
 use oat\taoWorkspace\model\lockStrategy\LockSystem;
@@ -38,6 +43,12 @@ use oat\taoWorkspace\model\RevisionWrapper;
  */
 class SetupWrapper extends InstallAction
 {
+    /**
+     * @param $params
+     *
+     * @throws common_Exception
+     * @throws InvalidServiceManagerException
+     */
     public function __invoke($params)
     {
         SqlStorage::createTable();
@@ -46,37 +57,42 @@ class SetupWrapper extends InstallAction
         $persistence = ModelManager::getModel()->getPersistence();
 
         /** @var common_persistence_Manager $pm */
-        $pm = $this->getServiceManager()->get(common_persistence_Manager::SERVICE_ID);
-        foreach ($pm->getOption(common_persistence_Manager::OPTION_PERSISTENCES) as $k => $p) {
+        $pm = $this->getServiceManager()->get(PersistenceManager::SERVICE_ID);
+        foreach ($pm->getOption(PersistenceManager::OPTION_PERSISTENCES) as $k => $p) {
             if ($persistence == $pm->getPersistenceById($k)){
                 break;
             }
         }
 
-        $workspaceModel = new core_kernel_persistence_smoothsql_SmoothModel(array(
-            core_kernel_persistence_smoothsql_SmoothModel::OPTION_PERSISTENCE => $k,
-            core_kernel_persistence_smoothsql_SmoothModel::OPTION_READABLE_MODELS => array($code),
-            core_kernel_persistence_smoothsql_SmoothModel::OPTION_WRITEABLE_MODELS => array($code),
-            core_kernel_persistence_smoothsql_SmoothModel::OPTION_NEW_TRIPLE_MODEL => $code,
-            core_kernel_persistence_smoothsql_SmoothModel::OPTION_SEARCH_SERVICE => ComplexSearchService::SERVICE_ID,
-        ));
-        
+        $workspaceModel = new SmoothModel(
+            [
+                SmoothModel::OPTION_PERSISTENCE => $k,
+                SmoothModel::OPTION_READABLE_MODELS => [$code],
+                SmoothModel::OPTION_WRITEABLE_MODELS => [$code],
+                SmoothModel::OPTION_NEW_TRIPLE_MODEL => $code,
+                SmoothModel::OPTION_SEARCH_SERVICE => ComplexSearchService::SERVICE_ID,
+            ]
+        );
+
         $model = ModelManager::getModel();
-        $model->setOption(core_kernel_persistence_smoothsql_SmoothModel::OPTION_SEARCH_SERVICE , ComplexSearchService::SERVICE_ID);
-        
-        $wrapedModel = WrapperModel::wrap($model, $workspaceModel );
-        $wrapedModel->setServiceLocator($this->getServiceLocator());
-        ModelManager::setModel($wrapedModel);
+        $model->setOption(SmoothModel::OPTION_SEARCH_SERVICE, ComplexSearchService::SERVICE_ID);
+
+        $wrappedModel = WrapperModel::wrap($model, $workspaceModel);
+        $wrappedModel->setServiceLocator($this->getServiceLocator());
+        ModelManager::setModel($wrappedModel);
 
         LockManager::setImplementation(new LockSystem());
 
-        $oldRepository = $this->getServiceManager()->get(Repository::SERVICE_ID);
+        $oldRepository = $this->getServiceManager()->get(RepositoryInterface::SERVICE_ID);
         $this->registerService('taoWorkspace/innerRevision', $oldRepository);
 
-        $newService = new RevisionWrapper(array(
-            RevisionWrapper::OPTION_INNER_IMPLEMENTATION => 'taoWorkspace/innerRevision',
-            RepositoryService::OPTION_FS => 'revisions'
-        ));
-        $this->registerService(Repository::SERVICE_ID, $newService);
+        $newService = new RevisionWrapper(
+            [
+                RevisionWrapper::OPTION_INNER_IMPLEMENTATION => 'taoWorkspace/innerRevision',
+                RepositoryService::OPTION_FILE_SYSTEM => 'revisions',
+            ]
+        );
+
+        $this->registerService(RepositoryInterface::SERVICE_ID, $newService);
     }
 }
